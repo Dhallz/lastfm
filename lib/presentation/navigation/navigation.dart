f8171976593186d22373ae3ca1eff7ce0c216589
Dhallz <dhawud@gmail.com>
2021-07-23 18:46:42 +0200

    Update Nav

--- lib/presentation/navigation/navigation.dart
+++ lib/presentation/navigation/navigation.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+// NOTE Usefull only for the 'settings' argument above.
+// import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lastfm/presentation/views/album_details/album_details_view.dart';
 import 'package:lastfm/presentation/views/album_details/album_details_view_controller_bindings.dart';
@@ -14,13 +15,18 @@ class Navigation {
           name: Routes.ALBUM_DETAILS,
           page: () => AlbumDetailsView(),
           binding: AlbumDetailsViewControllerBindings(),
-          settings: RouteSettings(name: Routes.ALBUM_DETAILS),
+          // NOTE Simple pre-setup line, note usefull a this point of the app. However it should work with it, on my side it does. Maybe restart vscode, or do:
+          // flutter upgrade
+          // flutter clean
+          // flutter pub get
+
+          // settings: RouteSettings(name: Routes.ALBUM_DETAILS),
         ),
         GetPage(
           name: Routes.HOME,
           page: () => HomeView(),
           binding: HomeViewControllerBindings(),
-          settings: RouteSettings(name: Routes.HOME),
+          // settings: RouteSettings(name: Routes.HOME),
         ),
       ];
 }
